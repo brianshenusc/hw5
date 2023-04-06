14b8b774a7379a1769f7e844fd305ff0d3d0af8e
Brian Shen <brianshe@usc.edu>
2023-04-06 01:16:51 +0000

    finished with schedwork

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,6 +20,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+
+// helper function that recursively fills out the schedule
 bool schedule_helper(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -27,18 +29,9 @@ bool schedule_helper(
     DailySchedule& sched, size_t day, int curr_worker, vector<Worker_T>& worker_shifts, unsigned int shifts_filled);
 
 
-
-// bool valid_worker(
-// 	size_t row, DailySchedule& sched, 
-// 	const size_t maxShifts, vector<Worker_T>& worker_shifts, 
-// 	const AvailabilityMatrix& avail, int curr_worker);
-
-
-
-void printSchedule(DailySchedule& sched);
-
-
 // Add your implementation of schedule() and other helper functions here
+
+
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -53,7 +46,7 @@ bool schedule(
     sched.clear();
     // Add your code below
     vector<Worker_T> worker_shifts;
-
+		// sets up the schedule 
     for(unsigned int i = 0; i < avail.size(); i++) {
         worker_shifts.push_back(0);
         vector<Worker_T> sched_row;
@@ -62,13 +55,15 @@ bool schedule(
         }
         sched.push_back(sched_row);
     }
-
+		// call to recursive function
     if(schedule_helper(avail, dailyNeed, maxShifts, sched, 0, 0, worker_shifts, 0)) {
         return true;
     }
     return false;
 }
 
+
+// helper function that recursively fills out the schedule
 bool schedule_helper(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -82,65 +77,23 @@ bool schedule_helper(
     for(size_t i = shifts_filled; i < avail[0].size(); i++) {
 		// if the current worker is valid on the current day and no duplicate workers on a day
 	    if(worker_shifts[i] < maxShifts && avail[day][i] && find(sched[day].begin(), sched[day].end(), i) == sched[day].end()) {
-
-			// adds the worker to the schedule and update information
-			sched[day][shifts_filled] = i;
-			worker_shifts[i]++;
-
-            // cout << "SCHEDULE AFTER: " << day << " " << shifts_filled << endl;
-            // printSchedule(sched);
-            // cout << endl; 
-            
-			// if the recursive call is true, return true
-			if(shifts_filled < sched[0].size()-1) {
-				// if there is still a shift to be shift to be filled
-				if(schedule_helper(avail, dailyNeed, maxShifts, sched, day, i, worker_shifts, shifts_filled+1)) {
+				// adds the worker to the schedule and update information
+				sched[day][shifts_filled] = i;
+				worker_shifts[i]++;
+				// if the recursive call is true, return true
+				if(shifts_filled < sched[0].size()-1) {
+					// if there is still a shift to be shift to be filled
+					if(schedule_helper(avail, dailyNeed, maxShifts, sched, day, i, worker_shifts, shifts_filled+1)) {
+						return true;
+					}
+				}
+				else if(schedule_helper(avail, dailyNeed, maxShifts, sched, day+1, i, worker_shifts, 0)) {
 					return true;
 				}
-			}
-      else if(schedule_helper(avail, dailyNeed, maxShifts, sched, day+1, i, worker_shifts, 0)) {
-				return true;
-      }
-			// otherwise remove the worker from the schedule
-
-      sched[day][shifts_filled] = INVALID_ID;
-      worker_shifts[i]--;
+				// otherwise remove the worker from the schedule
+				sched[day][shifts_filled] = INVALID_ID;
+				worker_shifts[i]--;
 			}
 		}
 		return false;
 }
-
-
-
-
-// bool valid_worker(
-// 	size_t row, DailySchedule& sched, 
-// 	const size_t maxShifts, vector<Worker_T>& worker_shifts, 
-// 	const AvailabilityMatrix& avail, int curr_worker) {
-
-// 		if(curr_worker == 1) {
-// 			cout << "1" << endl;
-// 		}
-// 		for(size_t i = 0; i < row; i++) {
-// 			for(size_t j = 0; j < sched[i].size(); j++) {
-// 				// return false if: the worker is unavailable OR has worked too many shifts
-// 				if(worker_shifts[curr_worker] >= maxShifts || !avail[i][curr_worker]) {
-// 					return false;
-// 				}
-// 			}
-// 		}
-// 		return true;
-// }
-
-
-
-
-
-// void printSchedule(DailySchedule& sched) {
-// 	for(unsigned int i = 0; i < sched.size(); i++) {
-// 		for(unsigned int j = 0; j < sched[i].size(); j++) {
-// 			cout << sched[i][j] << " ";
-// 		}
-// 		cout << endl;
-// 	}
-// }
